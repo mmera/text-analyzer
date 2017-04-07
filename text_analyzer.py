ca40161a7ef281d86bd00a0fc538aa74bb3da758
marcomera <marcomera@icloud.com>
2017-04-07 01:06:07 -0500

    Removed unnecessary import and fix PEP8 styling

--- text_analyzer.py
+++ text_analyzer.py
@@ -1,77 +1,78 @@
-from __future__ import print_function 
 from nltk.tokenize import sent_tokenize
 import operator
 import re
-import sys 
+import sys
 import io
 
+
 def parse_S_contraction(prevWord):
-	if re.match(r"([Ss]*[Hh]e$|[Ii]t$)", prevWord):
- 		return 'is'
- 	else: 
- 		return '\'s'
+    if re.match(r"([Ss]*[Hh]e$|[Ii]t$)", prevWord):
+        return 'is'
+    else:
+        return '\'s'
+
 
 def parse_contraction(prevWord, word):
- 	if word == 's':
- 		return parse_S_contraction(prevWord)
- 	else:
- 		return {
-        'd': 'would',
-        'll': 'will',
-        'm' : 'am',
-        're': 'are',
-        't': 'not',
-        've': 'have'
-    }.get(word, word)
+    if word == 's':
+        return parse_S_contraction(prevWord)
+    else:
+        return {
+            'd': 'would',
+            'll': 'will',
+            'm': 'am',
+            're': 'are',
+            't': 'not',
+            've': 'have'
+        }.get(word, word)
+
 
 def is_Contraction(word):
-	return word in ['d','ll','m','re','s','t','ve']
+    return word in ['d', 'll', 'm', 're', 's', 't', 've']
 
-if __name__ == "__main__": 
-	
-	file_to_read = sys.argv[1]
-	file_to_write = sys.argv[2]
-	
-	f = io.open(file_to_read, "r", encoding="utf-8")
-	text = f.read()
-	sys.stdout = open(file_to_write, 'w')
+if __name__ == "__main__":
 
-	word_list = re.findall(r"\w+[\.-]\w+[\.-]?\w*|\d+,\d+|\d*\.\d+|[\w]+|[-.,!?;:&$()\"]", text)
+    file_to_read = sys.argv[1]
+    file_to_write = sys.argv[2]
 
-	paragraph_count = len(re.findall(r"(\n\n\w)", text)) + 1 #RE finds gaps between paragraphs so add 1 to account for first paragraph  
-	token_count = {}
-	type_count = {} 
-	total_count = 0
- 
-	for index,word in enumerate(word_list):
-		try:
-			if word_list[index+1] == 't':
-				word = word[:-1] #Hacks off the 'n'; doesn't work for won't 
-		except IndexError:
-			break
-		if is_Contraction(word):
-			prevWord = word_list[index-1]
-			word = parse_contraction(prevWord, word)
-		if word not in token_count:
-			token_count[word] = 1
-			type_count[word.lower()] = 1
-			total_count += 1
-		else:
-			token_count[word] += 1
-			type_count[word.lower()] += 1
-			total_count += 1 
+    f = io.open(file_to_read, "r", encoding="utf-8")
+    text = f.read()
+    sys.stdout = open(file_to_write, 'w')
 
-	sort_dict_by_count = sorted(token_count.items(), key=operator.itemgetter(1), reverse=True)
+    word_list = re.findall(
+        r"\w+[\.-]\w+[\.-]?\w*|\d+,\d+|\d*\.\d+|[\w]+|[-.,!?;:&$()\"]", text)
 
-	print ('# of paragraphs = '+ str(paragraph_count))
-	print ('# of sentences = ' + str(len(sent_tokenize(text))))
-	print ('# of tokens: ' + str(total_count))
-	print ('# of types: ' + str(len(type_count)))
-	print ('=====================')
+    # RE finds gaps between paragraphs so add 1 to account for first paragraph
+    paragraph_count = len(re.findall(r"(\n\n\w)", text)) + 1
+    token_count = {}
+    type_count = {}
+    total_count = 0
 
-	for entry in sort_dict_by_count:
-		print (entry[0], entry[1])
- 	
+    for index, word in enumerate(word_list):
+        try:
+            if word_list[index + 1] == 't':
+                word = word[:-1]  # Hacks off the 'n'; doesn't work for "won't"
+        except IndexError:
+            break
+        if is_Contraction(word):
+            prevWord = word_list[index - 1]
+            word = parse_contraction(prevWord, word)
+        if word not in token_count:
+            token_count[word] = 1
+            type_count[word.lower()] = 1
+            total_count += 1
+        else:
+            token_count[word] += 1
+            type_count[word.lower()] += 1
+            total_count += 1
 
+    sort_dict_by_count = sorted(
+        token_count.items(), key=operator.itemgetter(1), reverse=True)
 
+    print '# of paragraphs = ' + str(paragraph_count)
+    print '# of sentences = ' + str(len(sent_tokenize(text)))
+    print '# of tokens: ' + str(total_count)
+    print '# of types: ' + str(len(type_count))
+    print '====================='
 
+    for entry in sort_dict_by_count:
+        print entry[0], entry[1]
